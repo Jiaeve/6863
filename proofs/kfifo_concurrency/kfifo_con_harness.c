adc9082b97923c2dd0fef73219da3d544f74dc5a
EveWu <EveWu@node1.xxx.cu-4824-f25-pg0.utah.cloudlab.us>
2025-12-17 14:18:53 -0700

    finish kfifo concurrency test

--- proofs/kfifo_concurrency/kfifo_con_harness.c
+++ proofs/kfifo_concurrency/kfifo_con_harness.c
@@ -1,84 +1,38 @@
-#include <pthread.h>
 #include <assert.h>
-#include "kfifo.h"
+#include "kfifo_cbmc.h"
 
-/*
- * CBMC nondeterministic functions
- * CBMC will treat these as symbolic values
- */
 unsigned char nondet_uchar(void);
 unsigned int nondet_uint(void);
 
-/*
- * Define a KFIFO with:
- *   element type: unsigned char
- *   capacity: 8
- */
 DEFINE_KFIFO(test_fifo, unsigned char, 8);
 
-/*
- * Producer thread:
- *   nondeterministically enqueue up to 2 elements
- */
-void *producer(void *arg) {
-    unsigned int n = nondet_uint();
-    __CPROVER_assume(n <= 2);
-
-    for (unsigned int i = 0; i < n; i++) {
-        unsigned char x = nondet_uchar();
-        kfifo_in(&test_fifo, &x, 1);
-    }
-    return NULL;
+static void prod_step(void) {
+  unsigned char x = nondet_uchar();
+  (void)kfifo_in(&test_fifo, &x, 1);
 }
 
-/*
- * Consumer thread:
- *   nondeterministically dequeue up to 2 elements
- */
-void *consumer(void *arg) {
-    unsigned int n = nondet_uint();
-    __CPROVER_assume(n <= 2);
-
-    for (unsigned int i = 0; i < n; i++) {
-        unsigned char y;
-        kfifo_out(&test_fifo, &y, 1);
-    }
-    return NULL;
+static void cons_step(void) {
+  unsigned char y;
+  (void)kfifo_out(&test_fifo, &y, 1);
 }
 
 int main(void) {
-    pthread_t t1, t2;
+  unsigned int steps = nondet_uint();
+  __CPROVER_assume(steps <= 6);
 
-    /*
-     * Create producer and consumer threads
-     */
-    pthread_create(&t1, NULL, producer, NULL);
-    pthread_create(&t2, NULL, consumer, NULL);
+  for (unsigned int i = 0; i < steps; i++) {
+    unsigned int choice = nondet_uint();
+    __CPROVER_assume(choice <= 1);
+    if (choice == 0) prod_step();
+    else cons_step();
+  }
 
-    /*
-     * Wait for both threads to finish
-     */
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+  unsigned int size = kfifo_size(&test_fifo);
+  unsigned int len  = kfifo_len(&test_fifo);
 
-    /*
-     * Post-condition checks
-     */
-    unsigned int size = kfifo_size(&test_fifo);
-    unsigned int len  = kfifo_len(&test_fifo);
+  assert(len <= size);
+  if (kfifo_is_full(&test_fifo))  assert(len == size);
+  if (kfifo_is_empty(&test_fifo)) assert(len == 0);
 
-    /* Length must never exceed capacity */
-    assert(len <= size);
-
-    /* Full FIFO implies len == size */
-    if (kfifo_is_full(&test_fifo)) {
-        assert(len == size);
-    }
-
-    /* Empty FIFO implies len == 0 */
-    if (kfifo_is_empty(&test_fifo)) {
-        assert(len == 0);
-    }
-
-    return 0;
+  return 0;
 }
